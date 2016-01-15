ad7450eeaaadd54642cb009ec7a5cf1e446413d3
jjensen <haiyun211@gmail.com>
2016-01-15 15:43:06 -0700

    Add product pages

---
+++ src/main/java/jjensen/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package jjensen.controllers;
+
+import jjensen.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jpjense3 on 1/15/2016.
+ */
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+}
---
+++ src/main/java/jjensen/services/ProductService.java
@@ -0,0 +1,12 @@
+package jjensen.services;
+
+import jjensen.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by jpjense3 on 1/15/2016.
+ */
+public interface ProductService {
+    List<Product> listAllProducts();
+}
